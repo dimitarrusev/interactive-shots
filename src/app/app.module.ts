c01d7311d817ee606ab24eb48f86e9e8f8ffc431
dimitarrusev <dimitarrusev@users.noreply.github.com>
2018-02-15 17:05:27 +0100

    build: install @angular/flex-layout, @angular/animations, @angular/material and @angular/cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
