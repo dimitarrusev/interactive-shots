c7dc45f0dc948be2ac518f9b3c7b2e8df249b074
dimitarrusev <dimitarrusev@users.noreply.github.com>
2018-02-16 17:33:26 +0100

    refactor(todo): refactor todo-register animations
    Rewrite todo-register animations to utilize the new animation utility functions.

--- src/app/todo/todo-register/todo-register.animations.ts
+++ src/app/todo/todo-register/todo-register.animations.ts
@@ -1,33 +1,94 @@
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { trigger, state, style, animate, transition, useAnimation } from '@angular/animations';
+import { generateSlideAnimation, fadeAnimation } from '../../shared/utils/animations';
 
-const easingCurves = {
-  standard: 'cubic-bezier(0.4, 0.0, 0.2, 1)',
-  deceleration: 'cubic-bezier(0.0, 0.0, 0.2, 1)',
-  acceleration: 'cubic-bezier(0.4, 0.0, 1, 1)',
-  sharp: 'cubic-bezier(0.4, 0.0, 0.6, 1)'
-};
+// Easing curves
+const standardEasingCurve = 'cubic-bezier(0.4, 0.0, 0.2, 1)';
+const decelerationEasingCurve = 'cubic-bezier(0.0, 0.0, 0.2, 1)';
+const accelerationEasingCurve = 'cubic-bezier(0.4, 0.0, 1, 1)';
+const sharpEasingCurve = 'cubic-bezier(0.4, 0.0, 0.6, 1)';
 
+// Animation parameters
+// slideInAnimation
+const slideInAnimationAnimateProperty = 'left';
+const slideInAnimationAnimatePropertyFrom = '800px';
+const slideInAnimationAnimatePropertyTo = '56px';
+const slideInAnimationDuration = '250ms';
+const slideInAnimationEasing = decelerationEasingCurve;
+const slideInAnimation = generateSlideAnimation(slideInAnimationAnimateProperty);
+
+// slideOutAnimation
+const slideOutAnimationAnimateProperty = 'left';
+const slideOutAnimationAnimatePropertyFrom = '56px';
+const slideOutAnimationAnimatePropertyTo = '-800px';
+const slideOutAnimationDuration = '350ms';
+const slideOutAnimationEasing = accelerationEasingCurve;
+const slideOutAnimation = generateSlideAnimation(slideOutAnimationAnimateProperty);
+
+// fadeOutAnimation
+const fadeOutAnimationFrom = 1;
+const fadeOutAnimationTo = 0;
+const fadeOutAnimationDuration = '350ms';
+const fadeOutAnimationEasing = decelerationEasingCurve;
+const fadeOutAnimation = fadeAnimation;
+
+// Animations
 const viewsContainerSlideInOutAnimation = () => {
   return trigger('viewsContainerSlideInOutAnimation', [
-    state('void', style({ left: '800px' })),
-    state('slide-in', style({ left: '56px' })),
-    state('slide-out', style({ left: '-800px' })),
-    transition('void => slide-in', [ animate(`250ms ${ easingCurves.deceleration }`) ]),
-    transition('slide-in => slide-out', [ animate(`350ms ${ easingCurves.acceleration }`) ])
+    state('void', style({
+      [slideInAnimationAnimateProperty]: slideInAnimationAnimatePropertyFrom
+    })),
+    transition('void => slide-in', [
+      useAnimation(slideInAnimation, {
+        params: {
+          from: slideInAnimationAnimatePropertyFrom,
+          to: slideInAnimationAnimatePropertyTo,
+          duration: slideInAnimationDuration,
+          easing: slideInAnimationEasing
+        }
+      }),
+    ]),
+    transition('slide-in => slide-out', [
+      useAnimation(slideOutAnimation, {
+        params: {
+          from: slideOutAnimationAnimatePropertyFrom,
+          to: slideOutAnimationAnimatePropertyTo,
+          duration: slideOutAnimationDuration,
+          easing: slideOutAnimationEasing
+        }
+      }),
+    ]),
   ]);
 };
 
 const defaultViewFadeOutAnimation = () => {
   return trigger('defaultViewFadeOutAnimation', [
-    state('fade-out', style({ opacity: '0' })),
-    transition('void => fade-out', [ animate(`350ms ${ easingCurves.deceleration }`) ]),
+    state('fade-out', style({ opacity: fadeOutAnimationTo })),
+    transition('void => fade-out', [
+      useAnimation(fadeOutAnimation, {
+        params: {
+          from: fadeOutAnimationFrom,
+          to: fadeOutAnimationTo,
+          duration: fadeOutAnimationDuration,
+          easing: fadeOutAnimationEasing
+        }
+      })
+    ]),
   ]);
 };
 
 const hoverViewFadeOutAnimation = () => {
   return trigger('hoverViewFadeOutAnimation', [
-    state('fade-out', style({ opacity: '0' })),
-    transition('void => fade-out', [ animate(`350ms ${ easingCurves.deceleration }`) ]),
+    state('fade-out', style({ opacity: fadeOutAnimationTo })),
+    transition('void => fade-out', [
+      useAnimation(fadeOutAnimation, {
+        params: {
+          from: fadeOutAnimationFrom,
+          to: fadeOutAnimationTo,
+          duration: fadeOutAnimationDuration,
+          easing: fadeOutAnimationEasing
+        }
+      })
+    ]),
   ]);
 };
 
